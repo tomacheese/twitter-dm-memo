01655dd1f8040b4b3741cc9b68dbde8dfee4caa3
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2023-01-25 07:46:22 +0900

    feat: v2
    BREAKING CHANGE: v2

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
-# TwitterDMMemo
+# twitter-dm-memo
 
 Forward the message you sent to yourself in Twitter Direct Message to Discord.
+
+## Installation
+
+Works in Node.js or Docker (Compose) environment.
+
+### Docker (Recommended)
+
+If you want to use Docker, write the following in `docker-compose.yml`:
+
+```yaml
+version: '3.8'
+
+services:
+  app:
+    image: ghcr.io/tomacheese/twitter-dm-memo:latest
+    volumes:
+      - type: bind
+        source: ./data
+        target: /data/
+    init: true
+    restart: always
+```
+
+After that, you can start it with `docker-compose up -d` after creating a configuration file with reference to [Configuration section](#configuration).
+
+### Node.js
+
+If you are running in a Node.js environment, Node.js v18 is recommended.
+
+Download `index.js` from the [release page](https://github.com/tomacheese/twitter-dm-memo/releases) in the latest release.  
+After that, you can start it with `node index.js` after creating a configuration file with reference to [Configuration section](#configuration).
+
+## Configuration
+
+The configuration file `data/config.json` is used by default.  
+If the environment variable `CONFIG_FILE` is set, the specified value is taken as the path to the configuration file.
+
+See here for the JSON Schema of the configuration file: [schema/Configuration.json](schema/Configuration.json)
+
+```json
+{
+  "$schema": "https://raw.githubusercontent.com/tomacheese/twitter-dm-memo/master/schema/Configuration.json"
+}
+```
+
+## License
+
+The license for this project is [MIT License](LICENSE).
---
+++ src/main.ts
@@ -0,0 +1,71 @@
+import {
+  DirectMessageCreateV1,
+  EDirectMessageEventTypeV1,
+  TwitterApi,
+} from 'twitter-api-v2'
+import { Notified } from './notified'
+import { loadConfig, loadTwitterApi, PATH, sendDiscordMessage } from './utils'
+
+async function getNotifiableDirectMessages(
+  twitterApi: TwitterApi,
+  notified: Notified
+): Promise<DirectMessageCreateV1[]> {
+  const me = await twitterApi.v1.verifyCredentials()
+
+  const events = await twitterApi.v1.listDmEvents({ count: 200 })
+
+  return events.events.filter((event) => {
+    return (
+      // メッセージ送信イベント
+      event.type === EDirectMessageEventTypeV1.Create &&
+      // 自分が送信したメッセージ
+      event[EDirectMessageEventTypeV1.Create].sender_id === me.id_str &&
+      // 自分宛てのメッセージ
+      event[EDirectMessageEventTypeV1.Create].target.recipient_id ===
+        me.id_str &&
+      // 既に通知済みのメッセージではない
+      !notified.isNotified(event.id)
+    )
+  })
+}
+
+async function main() {
+  const notified = new Notified(PATH.NOTIFIED_FILE)
+  const isFirst = notified.isFirst()
+  const config = loadConfig()
+  const twitterApi = loadTwitterApi(config)
+  const events = await getNotifiableDirectMessages(twitterApi, notified)
+
+  if (events.length === 0) {
+    console.log('❌ No new direct messages.')
+    notified.save()
+    return
+  }
+
+  console.log('✅ New direct messages found.')
+  for (const event of events) {
+    const directMessage = event[EDirectMessageEventTypeV1.Create]
+    const messageData = directMessage.message_data
+    let text = messageData.text
+    if (messageData.entities.urls) {
+      for (const url of messageData.entities.urls) {
+        text = text.replaceAll(url.url, url.expanded_url)
+      }
+    }
+
+    const sendText =
+      (config.discord.prefix ?? '') + text + (config.discord.suffix ?? '')
+
+    if (!isFirst) {
+      await sendDiscordMessage(config, sendText)
+    }
+
+    notified.add(event.id)
+  }
+
+  notified.save()
+}
+
+;(async () => {
+  await main()
+})()
